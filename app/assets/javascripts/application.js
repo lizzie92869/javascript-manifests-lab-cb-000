46347eb6f2ed4d607ae8a80b3a6cea365d0e59c3
Lizzie <lizzie.sorin@hotmail.fr>
2018-05-14 21:39:39 -0700

    Done.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require wrong_place_wrong_time
+//= require alert_maker
+//= require admin
+//= require main
